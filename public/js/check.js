507134cfa70d89fbb95cfc286ad96dacf07b942c
maciekkand <maciek.kand@gmail.com>
2019-07-01 01:19:43 +0200

    Check correctly verifies  user string input and photo is shown

--- public/js/check.js
+++ public/js/check.js
@@ -1,3 +1,35 @@
-const check = (string) => string === 'cattac';
+function isStringCatsOnly(str) {
+  const isStringCatsOnly = str
+    .replace(/cat/g, '')
+    .replace(/tac/g, '')
+    .replace(/CAT/g, '')
+    .replace(/TAC/g, '')
+    .replace(/CAT/g, '')
+    .replace(/cAt/g, '')
+    .replace(/tAc/g, '')
 
-export { check };
+  return isStringCatsOnly.length === 0
+}
+
+export const check = str => {
+  let len = str.length, i = 0, setLength = 6
+  
+  if (!len || (len % 6 !== 0)) return false // 'Error: incorrect number of letters' 
+  if (!isStringCatsOnly(str)) return false  // 'Error: incorrect substring(s)'
+
+  while(len) { 
+    const half1 = str.substr(i, setLength/2)
+    const reversedHalf2 = str.substr(i + setLength/2, setLength/2).split('').reverse().join('')
+
+    if(half1 === reversedHalf2) {
+      str = str.substr(setLength)
+      len = str.length
+      setLength = 6
+    }
+    else {
+      setLength = setLength + 6
+      if (i + setLength > len) return false
+    } 
+  }
+  return true
+}  
